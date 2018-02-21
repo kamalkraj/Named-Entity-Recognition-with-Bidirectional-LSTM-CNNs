69d742f5d8ea2f3445d4ca19b84f9e54b33bcf8a
kamal raj <kamalraj@cseonline.in>
2018-02-21 17:20:06 +0530

    Update prepro.py

--- prepro.py
+++ prepro.py
@@ -12,7 +12,7 @@ def readfile(filename):
     sentences = []
     sentence = []
     for line in f:
-        if len(line)==0 or line[0]=="-" or line[0]=="\n":
+        if len(line)==0 or line.startswith('-DOCSTART') or line[0]=="\n":
             if len(sentence) > 0:
                 sentences.append(sentence)
                 sentence = []
